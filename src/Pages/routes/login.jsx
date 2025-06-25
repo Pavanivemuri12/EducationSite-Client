934508d668ffb059269e489b828d3f3677a3b321
Pavani Sri Vemuri <153835795+Pavanivemuri12@users.noreply.github.com>
2025-06-25 23:06:45 +0530

    Update login.jsx

--- src/Pages/routes/login.jsx
+++ src/Pages/routes/login.jsx
@@ -5,8 +5,10 @@ export default function Login() {
     <div className="d-flex w-100 h-100 flex p-10 justify-center items-center">
       <SignIn
         signUpUrl="/sign-up"               // ✅ Used when the external account doesn't exist
-        redirectUrl="/"                    // ✅ Use this instead of forceRedireictUrl
+        redirectUrl="/"  
+        // ✅ Use this instead of forceRedireictUrl
       />
+      If External account not found click sign-up
     </div>
   );
 }
