595af4ef9157c5200e22dd156a93e4da2712080c
Pavani Sri Vemuri <153835795+Pavanivemuri12@users.noreply.github.com>
2025-06-25 23:17:05 +0530

    Update signup.jsx

--- src/Pages/routes/signup.jsx
+++ src/Pages/routes/signup.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react";
 
 export default function Signup() {
   return (
-    <div className="d-flex w-100 h-100 justify-center items-center">
+    <div className="d-flex w-100 h-100 flex p-10 justify-center items-center">
       <SignUp signInUrl="/login" afterSignUpUrl="/" />
     </div>
   );
