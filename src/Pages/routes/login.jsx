2fe6dd4dcaa21fdc5636ffadbd1562f62d5ecc3f
Pavani Sri Vemuri <153835795+Pavanivemuri12@users.noreply.github.com>
2025-06-25 23:07:51 +0530

    Update login.jsx

--- src/Pages/routes/login.jsx
+++ src/Pages/routes/login.jsx
@@ -3,12 +3,13 @@ import { SignIn } from "@clerk/clerk-react";
 export default function Login() {
   return (
     <div className="d-flex w-100 h-100 flex p-10 justify-center items-center">
+      If External account not found click sign-up
       <SignIn
         signUpUrl="/sign-up"               // ✅ Used when the external account doesn't exist
         redirectUrl="/"  
         // ✅ Use this instead of forceRedireictUrl
       />
-      If External account not found click sign-up
+      
     </div>
   );
 }
