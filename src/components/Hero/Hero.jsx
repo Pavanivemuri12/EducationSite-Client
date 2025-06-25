07dc6b5559de0b8ee26d1f2617e060925f90e7dc
Pavani Sri Vemuri <153835795+Pavanivemuri12@users.noreply.github.com>
2025-06-26 00:37:43 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,14 @@
 import { IoIosArrowRoundForward } from "react-icons/io";
-//import Navbar from "../Navbar/Navbar";
+// import Navbar from "../Navbar/Navbar";
 import blob from "../../assets/blob.svg";
 import hero from "../../assets/hero.png";
 import { motion } from "framer-motion";
 import Services from "../Services/Services";
-import Subscribe from "../Subscribe/Subscribe"
+import Subscribe from "../Subscribe/Subscribe";
 import Footer from "../Footer/Footer";
-import Banner from "../Banner/Banner"
-import Banner2 from "../Banner/Banner2"
+import Banner from "../Banner/Banner";
+import Banner2 from "../Banner/Banner2";
+import { Link } from "react-router-dom";
 
 export const FadeUp = (delay) => {
   return {
@@ -32,68 +33,70 @@ export const FadeUp = (delay) => {
 const Hero = () => {
   return (
     <>
-    <section className="bg-light overflow-hidden relative">
-      {/* <Navbar /> */}
-      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
-        {/* Brand Info */}
-        <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
-          <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
-            <motion.h1
-              variants={FadeUp(0.6)}
-              initial="initial"
-              animate="animate"
-              className="text-3xl lg:text-4xl font-bold !leading-snug"
-            >
-              Let&apos;s Learn and get the{" "}
-              <span className="text-secondary">best results </span>through smart
-              work
-            </motion.h1>
-            <motion.div
-              variants={FadeUp(0.8)}
-              initial="initial"
-              animate="animate"
-              className="flex justify-center md:justify-start"
-            >
-              <button className="primary-btn flex items-center gap-2 group text-2xl">
-                Get Started
-                <IoIosArrowRoundForward
-                  className="text-2xl group-hover:translate-x-4 
-                group-hover:rotate-90 duration-300"
-                />
-              </button>
-            </motion.div>
+      <section className="bg-light overflow-hidden relative">
+        {/* <Navbar /> */}
+        <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
+          {/* Brand Info */}
+          <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
+            <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
+              <motion.h1
+                variants={FadeUp(0.6)}
+                initial="initial"
+                animate="animate"
+                className="text-3xl lg:text-4xl font-bold !leading-snug"
+              >
+                Let&apos;s Learn and get the{" "}
+                <span className="text-secondary">best results </span>
+                through smart work
+              </motion.h1>
+              <motion.div
+                variants={FadeUp(0.8)}
+                initial="initial"
+                animate="animate"
+                className="flex justify-center md:justify-start"
+              >
+                <Link to="/courses">
+                  <button className="primary-btn flex items-center gap-2 group text-2xl">
+                    Get Started
+                    <IoIosArrowRoundForward
+                      className="text-2xl group-hover:translate-x-4 group-hover:rotate-90 duration-300"
+                    />
+                  </button>
+                </Link>
+              </motion.div>
+            </div>
+          </div>
+
+          {/* Hero image */}
+          <div className="flex justify-center items-center">
+            <motion.img
+              initial={{ x: 50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.4, ease: "easeInOut" }}
+              src={hero}
+              alt=""
+              className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"
+            />
+            <motion.img
+              initial={{ x: -50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
+              src={blob}
+              alt=""
+              className="absolute -bottom-32 w-[800px] md:w-[1500px] z-[1] hidden md:block"
+            />
           </div>
         </div>
-        {/* Hero image */}
-        <div className="flex justify-center items-center">
-          <motion.img
-            initial={{x:50, opacity: 0}}
-            animate={{x:0, opacity: 1}}
-            transition={{duration: 0.6, delay:0.4, ease: "easeInOut"}}
-            src={hero}
-            alt=""
-            className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"
-          />
-          <motion.img
-          initial={{x: -50, opacity: 0}}
-          animate={{x: 0, opacity:1}}
-          transition={{ duration: 0.6, delay:0.2, ease:"easeInOut"}}
-            src={blob}
-            alt=""
-            className="absolute -bottom-32 w-[800px] md:w-[1500px] z-[1] hidden md:block "
-          />
-        </div>
-      </div>
-    </section>
-    
- <Services/>
- <Banner/>
- <Subscribe/>
- <Banner2/>
- <Footer/>
- </>
+      </section>
+
+      {/* Other Sections */}
+      <Services />
+      <Banner />
+      <Subscribe />
+      <Banner2 />
+      <Footer />
+    </>
   );
- 
 };
 
 export default Hero;
